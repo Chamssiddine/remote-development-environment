216bab5215a48c317161c0caf0a03ec98c501f29
chamssiddine <chamseddine.abderrahim@gmail.com>
2023-06-01 17:04:34 +0100

    update jenkins file

--- infrastructure/modules/IamRbac/provider.tf
+++ infrastructure/modules/IamRbac/provider.tf
@@ -1,1 +1,30 @@
-# add provider if you will use this independently and not as a module
+terraform {
+  required_version = ">=1.3"
+  backend "gcs" {
+    bucket = "remotedevenv"
+    prefix = "terraform/state"
+  }
+
+}
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~>4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+
+  }
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+}

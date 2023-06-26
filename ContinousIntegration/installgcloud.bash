c9989664be2e1c41bf528a4b639ee58d42c8be38
chamssiddine <chamseddine.abderrahim@gmail.com>
2023-06-26 10:00:46 +0100

    update grafana values file to add persistent volume

--- ContinousIntegration/installgcloud.bash
+++ ContinousIntegration/installgcloud.bash
@@ -9,7 +9,7 @@ apt-get install -y apt-transport-https ca-certificates gnupg
 
 # Add Google Cloud SDK repository
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
+grep -rhE ^deb /etc/apt/sources.list* | grep "cloud-sdk"
 # Import Google Cloud public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
@@ -20,11 +20,12 @@ apt-get install -y google-cloud-sdk
 # Optional: Install additional components
 apt-get install kubectl
 apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+
 # apt-get install -y google-cloud-cli-anthos-auth
 # apt-get install -y google-cloud-cli-app-engine-go
 # apt-get install -y google-cloud-cli-app-engine-java
 # ...
-
+gcloud components install gke-gcloud-auth-plugin
 # Optional: Run gcloud init to configure the SDK
 # gcloud init
 
